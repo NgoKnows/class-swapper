a676bac9106ba3ab6b38ce8c6d9cb26c3d63bfec
Alex Ngo <alex_ngo@live.com>
2015-04-26 01:59:29 -0700

    created controller for home and create request pages

--- public/app/controllers/requestCtrl.js
+++ public/app/controllers/requestCtrl.js
@@ -1,3 +1,34 @@
 angular.module('requestCtrl', ['userService', 'requestService'])
 
-.controller('requestController', function(User, Request) {
+
+.controller('requestController', function (User, Request) {
+    var vm = this;
+
+    vm.processing = true;
+
+    Request.all()
+        .success(function (data) {
+            vm.processing = false;
+
+            vm.requests = data;
+        });
+    vm.deleteRequest = function(requestId){
+        Request.delete(requestId);
+    };
+})
+
+.controller('requestCreateController', function (User, Request, Auth) {
+    var vm = this;
+    vm.requestData = {};
+
+    //gets current username to attach to request add
+    vm.createRequest = function () {
+        vm.processing = true;
+        Request.add(vm.requestData)
+            .success(function(data){
+                vm.processing = false;
+                vm.message = data.message;
+                if(data.success) vm.requestData = {};
+        })
+    };
+});
